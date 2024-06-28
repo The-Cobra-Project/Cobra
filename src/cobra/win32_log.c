8a2c5b2211193acfb780ccdb2ee37a1785ab45ca
Ron Shani <ronshan4u@gmail.com>
2024-06-28 15:07:48 +0300

    logging

---
+++ src/cobra/win32_log.c
@@ -0,0 +1,11 @@
+#include "cobra/win32_log.h"
+
+#include "Windows.h"
+
+DWORD written = 0;
+
+void LogInfo(const char* msg) {
+    HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    WriteConsole(stdOut, msg, strlen(msg), &written, NULL);
+    written += strlen(msg);
+}
